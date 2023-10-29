db09d8fa1f64967dc98cbf91c07df0842a0a27c5
Kayla Sodergard <soderkk@gmail.com>
2023-10-29 00:03:54 -0400

    implemented basic teleop driving

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,7 +28,7 @@ public final class Constants {
     public static final int DRIVER_CONTROLLER = 1;
   }
 
-  public static final double ROBOT_MASS = Units.lbsToKilograms(90); // 32lbs * kg per pound
+  public static final double ROBOT_MASS = Units.lbsToKilograms(125); // 32lbs * kg per pound
   public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS); //where center of mass is
   public static final double LOOP_TIME = 0.13; // s, 20ms + 110ms sprk max velocity lag()
   public static final double DEFAULT_DEADBAND = 0.05;
